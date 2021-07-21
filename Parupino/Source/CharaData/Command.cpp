2a35b9389e863664e3a9a88807aca52b984c1bc8
AutumnWillow <yanagikeijiro@hotmail.com>
2021-07-21 16:45:34 +0900

    テキスト追加

--- Parupino/Source/CharaData/Command.cpp
+++ Parupino/Source/CharaData/Command.cpp
@@ -32,7 +32,7 @@ namespace GAME
 		UINT frame = ( m_limitTime < len ) ? m_limitTime : len;
 		
 		//完成入力数（カウントダウン）(判定は-1まで用いる)
-		int comp = m_vecGameKey.size() - 1;
+		int comp = (int)m_vecGameKey.size() - 1;
 
 		//フレームを遡って検索
 		for ( UINT i = 0; i < frame; ++i )
