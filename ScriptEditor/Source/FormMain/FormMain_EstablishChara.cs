0e272a2a9d6ab4e9cdf867a389b3b0d8d8ba7675
autumnwillow <yanagikeijiro@hotmail.com>
2022-09-21 17:53:31 +0900

    Merge commit '6eb90c5221393b3b859870b2a900cc77a833ba38' into HEAD
    # Conflicts:
    #	ScriptEditor/ScriptEditor.csproj
    #	ScriptEditor/Source/FormMain/FormMain.cs
    #	ScriptEditor/Source/FormMain/FormMain_Load.cs
    #	ScriptEditor/Source/FormMain/FormMain_menu.cs

---
+++ ScriptEditor/Source/FormMain/FormMain_EstablishChara.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ScriptEditor
+{
+	public partial class FormMain : Form
+	{
+		//==================================================================================
+		//	キャラ初期設定(の選択)
+		//==================================================================================
+		private void EstablishChara ()
+		{
+			//-------------------------------
+			//@info 下記いずれかを環境で選ぶ
+			//-------------------------------
+
+#if false
+			//キャラ：空データ
+			SetCharaData ( chara );
+
+			//キャラ：テストデータ
+			TestCharaData ();
+
+			//個別データから作成
+			LoadPreData ();
+#endif
+
+			//自動で既存のキャラデータを読込
+			_LoadChara ( stgs.LastFilepath );
+		}
+
+		//プレデータテキストファイルから既存データの読込
+		public void LoadPreData ()
+		{
+			ctrl_SqcList_Act.LoadData ();
+			ctrl_SqcList_Ef.LoadData ();
+			ctrl_CmdList1.LoadData ();
+			ctrl_Branch1.LoadData ();
+			ctrl_Route1.LoadData ();
+		}
+
+		//==================================================================================
+		//各コントロールのデータソースにキャラデータを設定する (読込時に１回のみ)
+		//==================================================================================
+		public void SetCharaData ( Chara ch )
+		{
+			//----------------------------------------------
+			EditChara.Inst.SetCharaDara ( ch );			//編集
+			DispChara.Inst.SetCharaData ( ch );			//表示
+
+			//----------------------------------------------
+			ctrl_SqcList_Act.SetCharaData ( ch.behavior );			//シークエンス
+			ctrl_SqcList_Ef.SetCharaData ( ch.garnish );			//シークエンス
+
+			//----------------------------------------------
+			cpd_Behavior.SetCharaData ( ch );			//コンペンド
+			cpd_Garnish.SetCharaData ( ch );
+
+			//----------------------------------------------
+			ctrl_CmdList1.SetCharaData ( ch );			//コマンド
+			ctrl_Branch1.SetCharaData ( ch );			//ブランチ
+			ctrl_Route1.SetCharaData ( ch );			//ルート
+
+			//----------------------------------------------
+			FormEfGnrt.Inst.SetCharaData ( ch );			// [エフェクトジェネレートフォーム]
+			FormRoute.Inst.SetCharaData ( ch );			//フォーム ルート
+
+			//----------------------------------------------
+			UpdateData ();			// [メインフォーム] キャラ内容表示
+		}
+
+	}
+}
