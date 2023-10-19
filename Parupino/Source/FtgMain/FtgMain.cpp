c9eabef756f750adc15447724bf2bbbb0093d0d1
YANAGI KEIJIROU <yanagikeijiro@hogmail.com>
2023-10-19 21:56:12 +0900

    Introduction : カーソル　HoldTimer導入、動作確認

--- Parupino/Source/FtgMain/FtgMain.cpp
+++ Parupino/Source/FtgMain/FtgMain.cpp
@@ -19,15 +19,19 @@ namespace GAME
 {
 	FtgMain::FtgMain ()
 	{
+#if 0
+
 		//戦闘
 		m_fighting = make_shared < Fighting > ();
 		AddpTask ( m_fighting );
 
+#endif // 0
 		//ポーズメニュ
 		m_pauseMenu = make_shared < PauseMenu > ();
 		AddpTask ( m_pauseMenu );
 
 		//@info コンストラクタでshared_from_this()を用いない
+
 	}
 
 	FtgMain::~FtgMain ()
@@ -36,54 +40,51 @@ namespace GAME
 
 	void FtgMain::Load ()
 	{
+
 //		m_scene = shared_from_this ();
 		m_gameScene = shared_from_this ();
 
+#if 0
 		m_pauseMenu->SetwpParent ( shared_from_this () );
+
+#endif // 0
 
 		Scene::Load ();
 	}
 
 	void FtgMain::ParamInit ()
 	{
+#if 0
+
 		m_fighting->ParamInit ( GetpParam () );
+
+#endif // 0
 	}
 
 
 	void FtgMain::Move ()
 	{
+#if 0
+		//メニュポーズ開始
+		if ( CFG_PUSH_KEY ( _P1_BTN5 ) || CFG_PUSH_KEY ( _P2_BTN5 ) )
+		{
+			m_pauseMenu->On ();
+		}
 
 		//メニュポーズ中
 		if ( m_pauseMenu->GetActive () )
 		{
 			m_pauseMenu->Move ();
-			return;
 		}
 
-		//メニュポーズ開始
-		if ( CFG_PUSH_KEY ( _P1_BTN5 ) || CFG_PUSH_KEY ( _P2_BTN5 ) )
-		{
-			m_pauseMenu->On ();
-			return;
-		}
-
-#if 0
-		//メニュポーズ中
-		if ( m_ynMenu->GetActive () )
-		{
-			m_ynMenu->Move ();
-			return;
-		}
-
-		//ポーズ開始
-		if ( CFG_PUSH_KEY ( _P1_BTN5 ) || CFG_PUSH_KEY ( _P2_BTN5 ) )
-		{
-			m_ynMenu->On ();
-			return;
-		}
-#endif // 0
 
 		//通常動作
+//		Scene::Move ();
+
+		//描画に必要なMove()はGRPLSTで行う
+
+		m_fighting->Move ();	//個別で手動
+#endif // 0
 		Scene::Move ();
 	}
 
@@ -91,12 +92,16 @@ namespace GAME
 	//状態遷移
 	P_GameScene FtgMain::Transit ()
 	{
+#if 0
+
 		//ESCで戻る
 		if ( ::GetAsyncKeyState ( VK_ESCAPE ) & 0x0001 )
 		{
 			SOUND->Stop ( BGM_Main );
 			return make_shared < Title > ();
 		}
+
+#endif // 0
 
 		//通常時
 //		return shared_from_this ();
@@ -120,7 +125,7 @@ namespace GAME
 //		GRPLST_INIT ();
 	}
 
-	//メニュを消して戻る
+	//メニュを消してゲームに戻る
 	void FtgMain::Resume_Fighting ()
 	{
 //		m_pauseMenu->Off ();
